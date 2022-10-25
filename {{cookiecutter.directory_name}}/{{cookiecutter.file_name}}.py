fb92b8d7f8c18543ba404eab5b459b4d593b1a29
BaDxKaRMa <bad87karma@gmail.com>
2022-10-25 00:09:43 -0400

    init commit

---
+++ {{cookiecutter.directory_name}}/{{cookiecutter.file_name}}.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import sys
+
+from dotenv import load_dotenv
+from loguru import logger
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--debug", action="store_true", default=False, help="Enable debug logging"
+    )
+    args = parser.parse_args()
+    # Remove all handlers
+    logger.remove()
+    # Set loguru format
+    fmt = (
+        "<green>{time:YYYY-MM-DD hh:mm:ss A}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{"
+        "function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> "
+    )
+
+    # Set Debug level if --debug is passed
+    if args.debug:
+        logger.add(sys.stderr, format=fmt, level="DEBUG")
+    else:
+        logger.add(sys.stderr, format=fmt, level="INFO")
+
+    # Load .env file
+    logger.debug("Loading .env file")
+    load_dotenv()
+
+    # Load environment variables
+    EXAMPLE_VARIABLE = os.getenv("EXAMPLE_VARIABLE")
+
+    # Start here
+    print("Hello World")
